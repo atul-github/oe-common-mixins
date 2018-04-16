c115cbcf5f12734b7202b2760c4fe819cb056cd2
Atul <atul_pandit@edgeverve.com>
2018-04-16 13:23:32 +0530

    adding HistoryMixin property to model-definition

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var app = require('oe-cloud');
+app.observe('loaded', function(ctx, next){
+  app.addSettingsToModelDefinition({properties : {HistoryMixin : {type : "boolean"}}});
+  return next();
+})
+
 module.exports = function(){
   
 }
