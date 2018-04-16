748193c51abf62043c3c311d3e240b60dad4489a
Atul <atul_pandit@edgeverve.com>
2018-04-16 15:47:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('oe-cloud');
 app.observe('loaded', function(ctx, next){
   app.addSettingsToModelDefinition({properties : {HistoryMixin : {type : "boolean"}}});
+  app.addSettingsToModelDefinition({properties : {_versioning : {type : "boolean", default : false}}});
   return next();
 })
 
